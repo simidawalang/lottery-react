f51607e889a9d8c843c11e5e3e97a81d31b733ac
simidawalang <simidawalang@gmail.com>
2022-02-23 10:07:05 +0100

    implemented test web3 lottery with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import lottery from "./lottery";
+import "./App.css";
+import web3 from "./web3";
 
 function App() {
+  //web3.eth.getAccounts().then(console.log);
+  const [manager, setManager] = useState("");
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState("");
+  const [etherValue, setEtherValue] = useState("");
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    const getManager = async () => {
+      const managerAddress = await lottery.methods.manager().call();
+      setManager(managerAddress);
+    };
+    getManager();
+  }, [manager]);
+
+  useEffect(() => {
+    const getLotteryPlayers = async () => {
+      const playerAddresses = await lottery.methods.getPlayers().call();
+      setPlayers(playerAddresses);
+    };
+    getLotteryPlayers();
+  }, []);
+
+  useEffect(() => {
+    const getContractBalance = async () => {
+      const contractBalance = await web3.eth.getBalance(
+        lottery.options.address
+      );
+      setBalance(contractBalance);
+    };
+    getContractBalance();
+  }, [balance]);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setEtherValue(value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    setMessage('Waiting on transaction success...');
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(etherValue, 'ether')
+    });
+
+    setMessage('You have been entered!');
+  }
+
+  const handlePickWinner =  async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    setMessage('Picking a winner...');
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    setMessage('The winner has been picked!')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lottery Contract</h2>
+      <p>This is managed by: {manager}</p>
+      <p>
+        There are currently {players.length}{" "}
+        {players.length === 1 ? "person" : "people"} entered in this lottery.
+      </p>
+      <p>
+        The money to be won is {web3.utils.fromWei(balance, "ether")} ether.
+      </p>
+      <form onSubmit={handleSubmit}>
+        <h4>Want to try? </h4>
+        <label htmlFor="ether-value">Amount of ether: </label>
+        <input
+          id="ether-value"
+          type="number"
+          onChange={handleChange}
+          value={etherValue}
+        />
+        <button>Enter</button>
+      </form>
+      <hr />
+      <h4>Ready to pick a winner?</h4>
+      <button onClick={handlePickWinner}>Pick Winner</button>
+      <hr />
+      <h1>{message}</h1>
     </div>
   );
 }
---
+++ src/lottery.js
@@ -0,0 +1,69 @@
+import web3 from "./web3";
+
+const address = "0xd2D9382C9091dc787B17cC749e3e7B6732137287";
+
+const ABI = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "randomNumber",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+
+export default new web3.eth.Contract(ABI, address);
+// Contract copy, not actually on the blockchain
